f9ca7ef0e588d8d250577e85c3f2f37cf22bd476
Ерикова Светлана <91692740+SvetaErikova@users.noreply.github.com>
2022-11-17 14:55:03 +0300

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,25 @@
 const swiper = new Swiper('.swiper', {
 
     direction: 'horizontal',
-    loop: true,
+
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        767: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    },
 
     navigation: {
         nextEl: '.swiper-button-next',
